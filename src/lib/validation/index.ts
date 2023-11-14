8c1d3134df84e312bbe19bda0d9b9cc755c5e316
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2023-11-14 12:40:48 +0530

    sign in

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -8,3 +8,10 @@ export const SignupValidation = z.object({
     .string()
     .min(2, { message: "Password must be at least 8 Characters." }),
 });
+
+export const SigninValidation = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(2, { message: "Password must be at least 8 Characters." }),
+});
