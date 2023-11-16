35cc4363d56c2e5dd98f86414c852e354731dfe0
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2023-11-16 20:40:37 +0530

    explore done

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -1,6 +1,13 @@
+import { useUserContext } from '@/context/AuthContext'
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Profile = () => {
+
+  const {id} = useParams()
+  const {user} =  useUserContext()
+  
+
   return (
     <div>Profile</div>
   )
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -81,6 +81,8 @@ export async function getCurrentUser() {
   }
 }
 
+
+
 export async function signOutAccount() {
   try {
     const session = await account.deleteSession("current");
@@ -317,6 +319,44 @@ export async function deletePost(postId: string, imageId: string) {
     );
 
     return { status: "ok" };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getInfinitePosts({ pageParam }: { pageParam: number }) {
+  const queries: any[] = [Query.orderDesc("$updatedAt"), Query.limit(10)];
+
+  if (pageParam) {
+    queries.push(Query.cursorAfter(pageParam.toString()));
+  }
+
+  try {
+    const posts = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      queries
+    );
+
+    if (!posts) throw Error;
+
+    return posts;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function searchPosts(searchTerm: string) {
+  try {
+    const posts = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      [Query.search("caption", searchTerm)]
+    );
+
+    if (!posts) throw Error;
+
+    return posts;
   } catch (error) {
     console.log(error);
   }
