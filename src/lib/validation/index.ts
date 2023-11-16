3fd69a7f26da94b31bec7f0023f6ef8f9f47ad90
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2023-11-16 12:42:36 +0530

    create post

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -15,3 +15,10 @@ export const SigninValidation = z.object({
     .string()
     .min(2, { message: "Password must be at least 8 Characters." }),
 });
+
+export const PostValidation = z.object({
+  caption: z.string().min(5).max(2200),
+  file: z.custom<File[]>(),
+  location: z.string().min(2).max(100),
+  tags: z.string(),
+});
